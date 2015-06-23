6a181900d05cf84b4b588caaecb65609cbab038a
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2015-06-23 13:26:26 +0100

    Add script to update a .po file from a version supplied by an editor that avoids messy re-ordering diff/merge etc

---
+++ scripts/updateLanguageFromSuppliedPO.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+MSGMERGE=`which msgmerge`
+
+if [ -z "$MSGMERGE" ]; then
+    echo "Error: unable to find msgmerge"
+    echo "GNU GetText Utilities must be installed and on the path to run this script"
+    exit 1
+fi
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+TRPATH="${SCRIPTPATH}/../src/RMP/Translate/lang/programmes"
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: updateLanguageFromSuppliedPO.sh new_english_translations.po en"
+    echo "Avoids lots of unnecessary messing around with diff/merge"
+    exit
+fi
+
+if [ ! -e "$1" ] ; then
+    echo "Error: File $1 not found";
+    exit 1
+fi
+if [ ! -e "$TRPATH/$2.po" ]; then
+    echo "Error: Existing translation at ${TRPATH}/$2.po does not exist";
+    exit 1;
+fi
+
+msgmerge -N -o - "$1" "$TRPATH/$2.po" | msgmerge -N -o "$TRPATH/$2.po" - "${TRPATH}/programmes.pot"
